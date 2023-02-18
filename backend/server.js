2c94b30d9c122dd948a96a0aa2b00415724fe749
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2023-02-19 00:50:14 +0530

    🐛 backend fixes

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,14 @@ const app = require('./app');
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database")
 
-// Config
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
 
+// Config
 dotenv.config({path: "backend/config/config.env"})
 
 // Connecting to database
@@ -12,4 +18,14 @@ connectDatabase()
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
 });
