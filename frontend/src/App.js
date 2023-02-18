eb2f915005bd5dcf9fb988ff76e75e32f64d0c97
vis <48868398+vis2x@users.noreply.github.com>
2023-02-18 21:20:12 +0000

    Created home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="">
-      <h1 className='text-xl'>Welcome to restyle market</h1>
-    </div>
-  );
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+export default function App() {
+	return (
+
+		<BrowserRouter>
+		<div className="App">
+			
+			<div className="content">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+				</Routes>
+		</div>
+
+		</div>
+		</BrowserRouter>
+	)
 }
 
-export default App;
