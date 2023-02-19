7a47a9badcb56dc966ff9ed06b3a13bfbf692e46
MarcoVmG <marco.vini.94@gmail.com>
2023-02-19 09:55:51 +0000

    Data js created

---
+++ frontend/src/data.js
@@ -0,0 +1,36 @@
+export default[
+    {
+        id: 1,
+        title:"test",
+        location:"London",
+        condition:"test",
+        price:"3.99",
+        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+    },
+    {
+        id: 2,
+        title:"test2",
+        location:"London",
+        condition:"test",
+        price:"3.99",
+        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+    },
+    {
+        id: 3,
+        title:"test3",
+        location:"Liverpool",
+        condition:"test",
+        price:"3.99",
+        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+    },
+    {
+        id: 4,
+        title:"test4",
+        location:"Liverpool",
+        condition:"test",
+        price:"3.99",
+        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+    },
+
+    
+]
