5e0f35c591265c274bdf1902f2eb240b4555f2d9
vis <48868398+vis2x@users.noreply.github.com>
2023-02-19 12:27:07 +0000

    Update data

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,36 +1,82 @@
-export default[
+export default [
     {
-        id: 1,
-        title:"test",
-        location:"London",
-        condition:"test",
-        price:"3.99",
-        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+        id: 1, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    }, 
+    {
+        id: 2, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
     },
     {
-        id: 2,
-        title:"test2",
-        location:"London",
-        condition:"test",
-        price:"3.99",
-        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+        id: 3, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
     },
     {
-        id: 3,
-        title:"test3",
-        location:"Liverpool",
-        condition:"test",
-        price:"3.99",
-        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+        id: 4, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    }, 
+    {
+        id: 5, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
     },
     {
-        id: 4,
-        title:"test4",
-        location:"Liverpool",
-        condition:"test",
-        price:"3.99",
-        image: "https://unsplash.com/es/fotos/WWpJViNdatc"
+        id: 6, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
     },
-
-    
+    {
+        id: 7, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    },
+    {
+        id: 8, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    },
+    {
+        id: 9, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    },
+    {
+        id: 10, 
+        title: "socks",
+        image: "https://i.imgur.com/xFDDpru.png",
+        location: "london",
+        description: "ok",
+        price: "£3.99"        
+    }
 ]
