7faf5ac2c6eedf88168627539e4b1477a97e3d0b
MarcoVmG <marco.vini.94@gmail.com>
2023-02-19 09:48:45 +0000

    Login form update

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -4,10 +4,10 @@ export default function LoginForm() {
   return (
     <div class=" grid place-items-center h-screen">
     <div className="grid place-items-center h-screen container--body">
-      <form>
+      <form action="/login" method="POST" noValidate>
         <div class="mb-3 xl:w-96">
             <label
-            for="exampleEmail0"
+            for="email"
             class="form-label inline-block mb-2 text-gray-700"
             >
             Email:
@@ -31,13 +31,14 @@ export default function LoginForm() {
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
             "
-            id="exampleEmail0"
+            id="email"
+            name="email"
             placeholder="Email"
             />
         </div>
         <div class="mb-3 xl:w-96">
             <label
-                for="examplePassword0"
+                for="password"
                 class="form-label inline-block mb-2 text-gray-700"
             >
                 Password:
@@ -61,12 +62,13 @@ export default function LoginForm() {
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                 "
-                id="examplePassword0"
+                id="password"
+                name="password"
                 placeholder="Password"
             />
             </div>
             <div class=" flex items-center justify-between ">
-            <button type="button" class="  inline-block px-6 py-2.5 bg-[#9873AC] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#9873AC] hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sig In</button>
+            <button type="button" class="inline-block px-6 py-2.5 bg-[#9873AC] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#9873AC] hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sig In</button>
             </div>
       </form>
       </div>
